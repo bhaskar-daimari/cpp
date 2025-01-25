428bff4dff48f06dfba511dfcd721bbc16410b66
bhaskar-daimari <156824811+bhaskar-daimari@users.noreply.github.com>
2025-01-25 17:45:42 +0530

    Add files via upload

---
+++ day3.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int main(){
+    cout << sqrt(16) << endl;
+    cout << log(10) << endl;
+    cout << max(3, 2) << endl;
+}
+
+
+// #include <iostream>
+// #include <string>
+// using namespace std;
+
+// int main(){
+//     // string firstName = "Bhaskar";
+//     // string lastName = "Daimari";
+//     // // cout << firstName + ' ' + lastName << endl;
+//     // firstName.append(lastName);
+//     // cout << firstName << endl;
+//     // // cout << 13 + "Name";
+//     // cout << firstName.size() << endl;
+//     // // cout << firstName[15] << endl;
+//     // int n=firstName.length();
+//     // firstName[0] = 'D';
+//     // for(int i=1; i<=n; i++){
+//     //     cout << firstName[n - i];
+//     // }
+//     // cout << endl;
+
+//     // int arr[] = {1,2,3,4,5};
+//     // cout << arr[0] << endl;
+//     string name;
+//     cout << "Enter your full name: ";
+//     getline(cin, name);
+//     cout << name << endl;
+//     return 0;
+// }
+
+// // << is called the insertion operator
+// // >> is called extraction operator
+
+// #include <iostream>
+// #include <string>
+
+// int main(){
+//     std::cout << "My name is Khan!" << std::endl;
+//     return 0;
+// }
