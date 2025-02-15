77bbb96abd5e59a77e28d5e3c72441767f6f2d32
bhaskar-daimari <156824811+bhaskar-daimari@users.noreply.github.com>
2025-02-15 22:55:37 +0530

    day11.cpp

---
+++ day11.cpp
@@ -0,0 +1,6 @@
+// We are taught to learn Time Complexity
+// Time complexity of merge sort is nlongn and it is the same with quicksort
+// However quicksort doesn't use extra space like mergesort
+// So in a sense it is better
+// To calculate time complexities of these algorithms we need to first find its recurrence relations
+// And solve the recurrence relations
